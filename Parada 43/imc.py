1c279ac5f3c7fbe8c1806a8e600032923ee8b67d
Nadson <nadsonsantossilva17@gmail.com>
2024-08-25 20:15:54 -0300

    change 'else' to 'elif'

--- Parada 43/imc.py
+++ Parada 43/imc.py
@@ -1,18 +1,15 @@
-altura = float(input("Digite sua altura em CM: "))
-peso = float(input("Digite seu peso: "))
-imc = peso / (altura*altura)
+height = float(input("Type your height, example(1.70): "))
+weight = float(input("Type your weight, example(60.5): "))
+imc = weight / (height*height)
 
 if imc < 18.5:
-    print("MAGREZA")
+    print("Underweight")
 
-elif imc >= 18.5 and imc <= 24.9:
-    print("NORMAL")
+elif imc >= 18.5 and imc <=24.9:
+    print("Nomral Weight")
 
-elif imc >= 25 and imc <= 29.9:
-    print("SOBREPESO")
+elif imc >= 25 and imc <=29.9:
+    print("Overweight")
 
-elif imc >= 30 and imc <= 39.9:
-    print("OBESIDADE")
-
-else:
-    print("OBESIDADE")
+elif imc >= 30:
+    print("Obesity")
